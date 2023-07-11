856560df4e0f111264bd88ec55726b152e48b0cd
Frederic.K <frederic.kreuder@gmail.com>
2023-07-11 20:31:07 +0200

    Working on userSlice

--- src/services/API/useUser.jsx
+++ src/services/API/useUser.jsx
@@ -3,17 +3,14 @@ import { createAsyncThunk } from '@reduxjs/toolkit'
 export const useUser = createAsyncThunk(
   'user/load',
   async ({ userId }, thunkAPI) => {
-    let url = `../../userMockedDatas/${userId}/user.json`
+    console.log('api')
+    let url = `./userMockedDatas/${userId}/user.json`
 
     try {
       const response = await fetch(url)
       let data = await response.json()
       console.log('user data', data, response.status)
-      if (response.status === 200) {
-        return { ...data }
-      } else {
-        return thunkAPI.rejectWithValue(data)
-      }
+      return { ...data }
     } catch (error) {
       console.log('Error', error.response.data)
       return thunkAPI.rejectWithValue(error.response.data)
