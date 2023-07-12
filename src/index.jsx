42f2288b857f7d8b82040872c9026f622c99ddd2
Frederic.K <frederic.kreuder@gmail.com>
2023-07-12 11:06:40 +0200

    Set background image

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import 'normalize.css'
+import './styles/style.scss'
 import { Toaster } from 'react-hot-toast'
 import App from './App'
 import store from './utils/store'
