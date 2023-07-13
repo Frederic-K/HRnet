9deb63a7b9daee3a3d7f9ca4761eddd6c9940dcc
Frederic.K <frederic.kreuder@gmail.com>
2023-07-13 20:45:21 +0200

    Set createEmployee page wireframe

--- src/pages/CreateEmployee/CreateEmployee.jsx
+++ src/pages/CreateEmployee/CreateEmployee.jsx
@@ -3,7 +3,19 @@ import Header from '../../components/Header/Header'
 export default function CreateEmployeeForm() {
   return (
     <div>
-      <Header />
+      Container
+      <div>
+        <Header />
+      </div>
+      <main>
+        <header>Create Employee</header>
+        <div>
+          Box Form
+          <div>Box info perso</div>
+          <div>Box Adresse</div>
+        </div>
+        <div>Box button</div>
+      </main>
     </div>
   )
 }
