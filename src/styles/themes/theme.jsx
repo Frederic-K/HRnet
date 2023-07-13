173e853e3c3ef72b99984f902253da824f149ec5
Frederic.K <frederic.kreuder@gmail.com>
2023-07-13 11:52:09 +0200

    Set Mui theme (color nd typo)

---
+++ src/styles/themes/theme.jsx
@@ -0,0 +1,13 @@
+import { createTheme } from '@mui/material/styles'
+import { green, teal } from '@mui/material/colors'
+
+export const globalTheme = createTheme({
+  typography: {
+    fontFamily: ['Archivo'].join(','),
+    fontSize: 17,
+  },
+  palette: {
+    primary: teal,
+    secondary: green,
+  },
+})
