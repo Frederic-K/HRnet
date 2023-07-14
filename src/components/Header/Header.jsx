fa726fff637dda41993b96859c05985aa63be3f2
Frederic.K <frederic.kreuder@gmail.com>
2023-07-14 15:11:14 +0200

    Working on Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,25 @@
+import { NavLink } from 'react-router-dom'
+
 import LiveDate from '../LiveDate/LiveDate'
 import LiveClock from '../LiveClock/LiveClock'
 
+import Logo from '../../assets/WealthHealth_Logo_1.png'
+
 export default function Header() {
   return (
-    <div className="header">
-      <div className="header__liveDate">
-        <LiveDate />
+    <header className="header">
+      <div className="header__period">
+        <div className="header__period--liveDate">
+          <LiveDate />
+        </div>
+        <div className="header__period__liveClock">
+          <LiveClock />
+        </div>
       </div>
-      <div className="header__liveClock">
-        <LiveClock />
+      <div className="header__logo">
+        <img src={Logo} alt="Logo Wealth Health" />
       </div>
       <div className="header__title">WHEALTH HEALTH</div>
-    </div>
+    </header>
   )
 }
