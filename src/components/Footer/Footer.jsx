9fd03a8ec7b4df421a720683a30f413196c78cac
Frederic.K <frederic.kreuder@gmail.com>
2023-07-20 14:05:54 +0200

    Implement logout

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,15 +1,42 @@
+import { useNavigate } from 'react-router-dom'
+import { useDispatch } from 'react-redux'
+import { clearState } from '../../features/userSlice'
+
+import Button from '@mui/material/Button'
+import ExitToAppIcon from '@mui/icons-material/ExitToApp'
+
 export default function Footer() {
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+
+  const handelLogoutClick = () => {
+    dispatch(clearState())
+    navigate('/')
+  }
+
   return (
     <footer className="footer">
-      <a className="footer__mail" href="mailto:contact@wealthhealth.com">
-        Contact
-      </a>
       <div className="footer__copyright">
         Copyright @ {new Date().getFullYear()} Whealth Health
       </div>
-      <a className="footer__mail" href="mailto:assistance@wealthhealth.com">
-        Assistance
-      </a>
+      <div>
+        <a className="footer__mail" href="mailto:contact@wealthhealth.com">
+          Contact
+        </a>
+        <a className="footer__mail" href="mailto:assistance@wealthhealth.com">
+          Assistance
+        </a>
+        <Button
+          variant="outlined"
+          size="small"
+          className="footer__logout"
+          onClick={() => {
+            handelLogoutClick()
+          }}
+        >
+          <ExitToAppIcon className="footer__logout--btn" />
+        </Button>
+      </div>
     </footer>
   )
 }
