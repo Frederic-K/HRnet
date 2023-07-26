34b73ff7d79c18b9f3a868291bf62b2b019d24ce
Frederic.K <frederic.kreuder@gmail.com>
2023-07-26 14:31:51 +0200

    Clean employeeSlice

--- src/features/employeeSlice.js
+++ src/features/employeeSlice.js
@@ -4,9 +4,9 @@ const initialState = {
   employees: [],
 }
 
-let arrayEmployees = JSON.parse(localStorage.getItem('employees')) || []
-localStorage.setItem('employees', JSON.stringify(arrayEmployees))
-arrayEmployees = JSON.parse(localStorage.getItem('employees'))
+// let arrayEmployees = JSON.parse(localStorage.getItem('employees')) || []
+// localStorage.setItem('employees', JSON.stringify(arrayEmployees))
+// arrayEmployees = JSON.parse(localStorage.getItem('employees'))
 
 export const employeesSlice = createSlice({
   name: 'employees',
@@ -41,38 +41,3 @@ export const { addEmployee, deleteEmployee, clearEmployeeState } =
 export const employeeSelector = (state) => state.employees
 
 export default employeesSlice.reducer
-
-// import { createSlice } from '@reduxjs/toolkit'
-
-// const initialState = {
-//   employees: [],
-// }
-
-// export const employeesSlice = createSlice({
-//   name: 'employees',
-//   initialState,
-//   reducers: {
-//     addEmployee: (state, action) => {
-//       state.employees.push(action.payload)
-//     },
-//     deleteEmployee: (state, action) => {
-//       console.log('action.payload', action.payload)
-//       // state.employees.filter((el) => el.employeeID !== action.payload)
-//       state.employees.filter((el) => !action.payload.includes(el.employeeID))
-//       // console.log('employees-state', state.employees)
-//     },
-//     clearEmployeeState: () => {
-//       // localStorage.removeItem('employees')
-//       return {
-//         employees: [],
-//       }
-//     },
-//   },
-// })
-
-// export const { addEmployee, deleteEmployee, clearEmployeeState } =
-//   employeesSlice.actions
-
-// export const employeeSelector = (state) => state.employees
-
-// export default employeesSlice.reducer
