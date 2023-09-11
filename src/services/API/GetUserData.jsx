99189ddece103dd107c107544190b7139e740aec
Frederic.K <frederic.kreuder@gmail.com>
2023-09-11 13:06:46 +0200

    Clean API

--- src/services/API/GetUserData.jsx
+++ src/services/API/GetUserData.jsx
@@ -3,13 +3,10 @@ import { createAsyncThunk } from '@reduxjs/toolkit'
 export const GetUserData = createAsyncThunk(
   'user/load',
   async (userId, thunkAPI) => {
-    // console.log('api')
     let url = `./userMockedDatas/${userId}/user.json`
-    //console.log('url', url)
     try {
       const response = await fetch(url)
       let data = await response.json()
-      // console.log('user data', data, response.status)
       if (response.status === 200) {
         localStorage.setItem('userID', data.userData.id)
         localStorage.setItem('userLastName', data.userData.lastName)
